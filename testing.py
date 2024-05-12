57b578bf2a39cbb9d0890ba6b65388f2d1d96c88
Shrinkhal <97280075+Shrinkhal01@users.noreply.github.com>
2024-05-12 15:32:11 +0530

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,74 +1,40 @@
-import os
-import numpy as np
 import cv2
-import tensorflow as tf
 
-# Replace with your actual model path
-model_path = "folder path where you save this repo"
+thres = 0.50
 
-pb_file = os.path.join(model_path, 'frozen_inference_graph.pb')
+cap = cv2.VideoCapture(0)#this line captures the video from the webcam
+cap.set(3, 1850)#this line width
+cap.set(4, 1850)#setting height
+cap.set(10, 70)#setting brightness
 
-# Read the graph
-try:
-    with tf.io.gfile.GFile(pb_file, 'rb') as f:
-        graph_def = tf.compat.v1.GraphDef()
-        graph_def.ParseFromString(f.read())
-except tf.errors.NotFoundError:
-    print(f"Error: Unable to find the file {pb_file}. Make sure the path is correct.")
-    exit()
+classNames = []#creating empty name of classes
+classFile = 'coco.names'#file with the names of the classes
+with open(classFile, 'rt') as f:#storing thte names of the classes in the list
+    classNames = f.read().rstrip('\n').split('\n')
 
-# Configuring GPU memory growth
-gpus = tf.config.experimental.list_physical_devices('GPU')
-if gpus:
-    try:
-        for gpu in gpus:
-            tf.config.experimental.set_memory_growth(gpu, True)
-    except RuntimeError as e:
-        print(e)
+# following are config files for the model
+configPath = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
+weightsPath = 'frozen_inference_graph.pb'
 
-with tf.compat.v1.Session() as sess:
-    sess.graph.as_default()
-    tf.import_graph_def(graph_def, name='')
+net = cv2.dnn_DetectionModel(weightsPath, configPath)
+net.setInputSize(320, 320)
+net.setInputScale(1.0 / 127.5)
+net.setInputMean((127.5, 127.5, 127.5))
+net.setInputSwapRB(True)
 
-    # Open the webcam
-    cap = cv2.VideoCapture(0)
+while True:
+    success, img = cap.read()
+    classIds, confs, bbox = net.detect(img, confThreshold=thres)
+    print(classIds, bbox)
 
-    while True:
-        ret, frame = cap.read()
+    if len(classIds) != 0:
+        for classId, confidence, box in zip(classIds.flatten(), confs.flatten(), bbox):
+            if 0 <= classId - 1 < len(classNames):
+                cv2.rectangle(img, box, color=(0, 255, 0), thickness=2)
+                cv2.putText(img, classNames[classId - 1].upper(), (box[0] + 10, box[1] + 30),
+                            cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+                cv2.putText(img, str(round(confidence * 100, 2)), (box[0] + 200, box[1] + 30),
+                            cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
 
-        if not ret:
-            print("Error reading frame")
-            break
-
-        # Run the model on the frame
-        img_in = frame[:, :, [2, 1, 0]]  # BGR2RGB
-        outputs = sess.run([sess.graph.get_tensor_by_name('num_detections:0'),
-                            sess.graph.get_tensor_by_name('detection_scores:0'),
-                            sess.graph.get_tensor_by_name('detection_boxes:0'),
-                            sess.graph.get_tensor_by_name('detection_classes:0')],
-                           feed_dict={'image_tensor:0': img_in.reshape(1, img_in.shape[0], img_in.shape[1], 3)})
-
-        # Visualize the results
-        font = cv2.FONT_HERSHEY_SIMPLEX
-
-        num_detections = int(outputs[0][0])
-        for i in range(num_detections):
-            classId = int(outputs[3][0][i])
-            score = float(outputs[1][0][i])
-            bbox = [float(v) for v in outputs[2][0][i]]
-
-            x = bbox[1] * frame.shape[1]
-            y = bbox[0] * frame.shape[0]
-            right = bbox[3] * frame.shape[1]
-            bottom = bbox[2] * frame.shape[0]
-
-            cv2.rectangle(frame, (int(x), int(y)), (int(right), int(bottom)), (225, 255, 0), thickness=2)
-            cv2.putText(frame, str(classId), (int(x), int(y)), font, 1, (200, 0, 0), 3, cv2.LINE_AA)
-
-        cv2.imshow("Camera Feed", frame)
-
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    cap.release()
-    cv2.destroyAllWindows()
+    cv2.imshow('Output', img)
+    cv2.waitKey(1)
